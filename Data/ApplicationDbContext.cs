f4f509193879d701c0cc551b7ae0cc6c71cc1f01
ITNikolov <ivaylonikolov.n@gmail.com>
2025-03-16 20:24:01 +0200

    fix identity migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,17 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using OnlineBookStore.Models;
 
 namespace OnlineBookStore.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
+
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-
     }
 }
