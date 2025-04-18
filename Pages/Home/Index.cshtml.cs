37eb6e97f46bbab5e0e686f2a34c850fbac4a94a
ITNikolov <ivaylonikolov.n@gmail.com>
2025-04-18 19:11:14 +0300

    home page made and added some styles to keep the picture clean and full size removed old index swapped pathways to replace the old index

--- Pages/Home/Index.cshtml.cs
+++ Pages/Home/Index.cshtml.cs
@@ -1,12 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.EntityFrameworkCore;
+using OnlineBookStore.Data;
+using OnlineBookStore.Models;
 namespace OnlineBookStore.Pages.Home
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public List<Product> Featured { get; set; } = new List<Product>();
+
+        public async Task OnGetAsync()
+        {
+            Featured = await _db.Products
+                .Include(p => p.Category)      
+                .OrderBy(p => p.Title)         
+                .Take(25)                      
+                .ToListAsync();
         }
     }
 }
