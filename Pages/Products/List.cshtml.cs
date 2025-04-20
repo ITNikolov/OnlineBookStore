2baf3a54b32c9419b9e7a760df6369f1ad980f0f
ITNikolov <ivaylonikolov.n@gmail.com>
2025-04-20 21:02:13 +0300

    layout of list page done implemented basic ui search is coming soon

--- Pages/Products/List.cshtml.cs
+++ Pages/Products/List.cshtml.cs
@@ -1,12 +1,28 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OnlineBookStore.Data;
+using OnlineBookStore.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace OnlineBookStore.Pages.Products
 {
     public class ListModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        public ListModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public async Task OnGetAsync()
+        {
+            Products = await _db.Products
+                                .Include(p => p.Category)
+                                .ToListAsync();
         }
     }
 }
