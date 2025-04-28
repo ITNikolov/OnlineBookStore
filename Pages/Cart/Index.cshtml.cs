9cd73d81ce037b110869db239d5e69dd84c24714
ITNikolov <ivaylonikolov.n@gmail.com>
2025-04-28 23:15:30 +0300

    index page model and view created for cart

--- Pages/Cart/Index.cshtml.cs
+++ Pages/Cart/Index.cshtml.cs
@@ -1,12 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineBookStore.Utility;
+using OnlineBookStore.Models;
 
 namespace OnlineBookStore.Pages.Cart
 {
     public class IndexModel : PageModel
     {
+        public List<CartItem> CartItems { get; set; } = new();
+
         public void OnGet()
         {
+            CartItems = HttpContext.Session.Get<List<CartItem>>("Cart") ?? new List<CartItem>();
         }
     }
 }
