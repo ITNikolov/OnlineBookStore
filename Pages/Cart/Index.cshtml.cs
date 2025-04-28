2b8fd2de169fea55d40cbe474b2f85fbaf8029dc
ITNikolov <ivaylonikolov.n@gmail.com>
2025-04-28 23:25:26 +0300

    injected database in the cart page model to be able to read data for the product

--- Pages/Cart/Index.cshtml.cs
+++ Pages/Cart/Index.cshtml.cs
@@ -1,16 +1,29 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OnlineBookStore.Utility;
 using OnlineBookStore.Models;
+using OnlineBookStore.Data; 
 
 namespace OnlineBookStore.Pages.Cart
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db; 
+
         public List<CartItem> CartItems { get; set; } = new();
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
         public void OnGet()
         {
             CartItems = HttpContext.Session.Get<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+            foreach (var item in CartItems)
+            {
+                item.Product = _db.Products.FirstOrDefault(p => p.Id == item.ProductId);
+            }
         }
     }
 }
