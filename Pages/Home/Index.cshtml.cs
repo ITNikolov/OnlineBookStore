9d46cdd44c627b6d73fccff259c01a651cc928f3
ITNikolov <ivaylonikolov.n@gmail.com>
2025-05-05 13:25:00 +0300

    implemented simple pagination seperating pages by 10 books per page

--- Pages/Home/Index.cshtml.cs
+++ Pages/Home/Index.cshtml.cs
@@ -8,6 +8,10 @@ namespace OnlineBookStore.Pages.Home
     {
         private readonly ApplicationDbContext _db;
 
+        public const int PageSize = 10;
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+
         public IndexModel(ApplicationDbContext db)
         {
             _db = db;
@@ -15,12 +19,18 @@ namespace OnlineBookStore.Pages.Home
 
         public List<Product> Featured { get; set; } = new List<Product>();
 
-        public async Task OnGetAsync()
+
+        public async Task OnGetAsync(int pageNumber = 1)
         {
+            var totalProducts = await _db.Products.CountAsync();
+            TotalPages = (int)Math.Ceiling(totalProducts / (double)PageSize);
+            CurrentPage = pageNumber;
+
             Featured = await _db.Products
-                .Include(p => p.Category)      
-                .OrderBy(p => p.Title)         
-                .Take(25)                      
+                .Include(p => p.Category)
+                .OrderBy(p => p.Title)
+                .Skip((pageNumber - 1) * PageSize)
+                .Take(PageSize)
                 .ToListAsync();
         }
     }
