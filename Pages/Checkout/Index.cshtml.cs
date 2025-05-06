c28279e3391d32c1053803217578c37407ea3c2c
ITNikolov <ivaylonikolov.n@gmail.com>
2025-05-06 13:56:48 +0300

    deleted some books to add more beautiful book covers and fixed error message box always showing

--- Pages/Checkout/Index.cshtml.cs
+++ Pages/Checkout/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineBookStore.Data;
 using OnlineBookStore.Models;
 using OnlineBookStore.Utility;
 
@@ -7,6 +8,13 @@ namespace OnlineBookStore.Pages.Checkout
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         [BindProperty]
         public CheckoutViewModel CheckoutForm { get; set; } = new();
 
@@ -14,24 +22,35 @@ namespace OnlineBookStore.Pages.Checkout
 
         public void OnGet()
         {
-            CheckoutForm.CartItems = HttpContext.Session.Get<List<CartItem>>("Cart") ?? new();
-
-            foreach (var item in CheckoutForm.CartItems)
-            {
-                TotalAmount += (decimal)item.Product.ListPrice * item.Quantity;
-            }
+            LoadCartAndCalculateTotal();
         }
 
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
             {
+                LoadCartAndCalculateTotal();
                 return Page();
             }
 
             HttpContext.Session.Remove("Cart");
             TempData["success"] = "Your order has been placed!";
             return RedirectToPage("/Home/Index");
+        }
+
+        private void LoadCartAndCalculateTotal()
+        {
+            var cart = HttpContext.Session.Get<List<CartItem>>("Cart") ?? new();
+            foreach (var item in cart)
+            {
+                item.Product = _db.Products.FirstOrDefault(p => p.Id == item.ProductId);
+                if (item.Product != null)
+                {
+                    TotalAmount += (decimal)item.Product.ListPrice * item.Quantity;
+                }
+            }
+
+            CheckoutForm.CartItems = cart;
         }
     }
 }
