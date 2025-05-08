95c49495cad4ab3f4ed59fcc218d62055ee7d147
ITNikolov <ivaylonikolov.n@gmail.com>
2025-05-08 23:36:00 +0300

    index order reworked having details delete and image showing with additional info setup for details page to be handles later

--- Pages/Order/Details.cshtml.cs
+++ Pages/Order/Details.cshtml.cs
@@ -1,10 +1,10 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace OnlineBookStore.Pages.Order
 {
     public class DetailsModel : PageModel
     {
+        public List<Models.Order> Orders { get; set; } = new();
         public void OnGet()
         {
         }
