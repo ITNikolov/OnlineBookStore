c8c01d82168e5da2697293e4b4b685238511b7ef
ITNikolov <ivaylonikolov.n@gmail.com>
2025-05-08 21:38:30 +0300

    fixed view and model making orders visiable and added layout path to access my orders

--- Pages/Order/Index.cshtml.cs
+++ Pages/Order/Index.cshtml.cs
@@ -1,12 +1,36 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OnlineBookStore.Data;
+using OnlineBookStore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace OnlineBookStore.Pages.Order
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public List<Models.Order> Orders { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            var userId = User.Identity?.Name;
+            if (!string.IsNullOrEmpty(userId))
+            {
+                Orders = await _db.Orders
+                    .Where(o => o.UserId == userId)
+                    .Include(o => o.Items)
+                    .ThenInclude(i => i.Product)
+                    .OrderByDescending(o => o.OrderDate)
+                    .ToListAsync();
+            }
         }
     }
 }
