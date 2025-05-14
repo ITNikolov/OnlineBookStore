2aa146c8678d16c8476305f93f0ce8b810b6afba
ITNikolov <ivaylonikolov.n@gmail.com>
2025-05-14 22:05:29 +0300

    removed frontend pagination used paginationlist class to make reuseable global pagination that can be reused in views

--- Pages/Home/Index.cshtml.cs
+++ Pages/Home/Index.cshtml.cs
@@ -2,36 +2,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using OnlineBookStore.Data;
 using OnlineBookStore.Models;
+using OnlineBookStore.Utility;
 namespace OnlineBookStore.Pages.Home
 {
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _db;
-
-        public const int PageSize = 10;
-        public int CurrentPage { get; set; }
-        public int TotalPages { get; set; }
+        public PaginatedList<Product> Featured { get; set; }
 
         public IndexModel(ApplicationDbContext db)
         {
             _db = db;
         }
 
-        public List<Product> Featured { get; set; } = new List<Product>();
-
-
-        public async Task OnGetAsync(int pageNumber = 1)
+        public async Task OnGetAsync(int? pageIndex)
         {
-            var totalProducts = await _db.Products.CountAsync();
-            TotalPages = (int)Math.Ceiling(totalProducts / (double)PageSize);
-            CurrentPage = pageNumber;
+            int pageSize = 10;
+            var query = _db.Products
+                .Include(j => j.Category)
+                .OrderBy(j => j.Title)
+                .AsNoTracking();
 
-            Featured = await _db.Products
-                .Include(p => p.Category)
-                .OrderBy(p => p.Title)
-                .Skip((pageNumber - 1) * PageSize)
-                .Take(PageSize)
-                .ToListAsync();
+            Featured = await PaginatedList<Product>
+                .CreateAsync(query, pageIndex ?? 1, pageSize);
         }
     }
 }
